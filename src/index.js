94aafc9a7c2eb2d9ee3c6e530768ed5409deacdd
Curt Bryan <kurdtbryan@gmail.com>
2019-06-24 08:34:15 -0700

    done for day 6-20-19

--- src/index.js
+++ src/index.js
@@ -7,16 +7,7 @@ import { Provider } from "react-redux";
 import store from "./Ducks/store";
 import * as serviceWorker from "./serviceWorker";
 import * as firebase from "firebase";
-const config = {
-  apiKey: "AIzaSyCAYXVRKge-TCOkcIi5Roe_ADd9jwNn694",
-  authDomain: "yhh-phx.firebaseapp.com",
-  databaseURL: "https://yhh-phx.firebaseio.com",
-  projectId: "yhh-phx",
-  storageBucket: "",
-  messagingSenderId: "757751039585",
-  appId: "1:757751039585:web:7a0e0fa468371ccf"
-};
-firebase.initializeApp(config);
+
 
 ReactDOM.render(
   <BrowserRouter>
